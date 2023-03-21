07efb0dc668a09ac0098e192540561570243e8d5
IanKiguru <iankiguru72@gmail.com>
2023-03-21 09:40:22 -0700

    new

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,34 +7,29 @@ void times_table(void)
 {
 int i, j, k;
 
-for (i = 0; i <= 9; i++)
-{
-	for (j = 0; j <= 9; j++)
+	for (i = 0; i < 10; i++)
 	{
-		k = i * j;
+		for (j = 0; j < 10; j++)
+		{
+			k = j * i;
 
-		if (j == 0)
-			_putchar('0');
-		else if (k <= 9)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(k + '0');
+			if (j == 0)
+				_putchar(k + '0');
+
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
 		}
-		else
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar((k + 10) + '0');
-			_putchar((k % 10) + '0');
-		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-}
-
-
-
-
-
 }
