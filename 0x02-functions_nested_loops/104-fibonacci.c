21efd325b9fdb39179a78d6107a816334042aeff
IanKiguru <iankiguru72@gmail.com>
2023-03-21 11:50:49 -0700

    new

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -31,12 +31,18 @@ int main(void)
 
 	for (i = 92; i < 99; i++)
 	{
-		printf(", %lu", aft1 + (aft2 / l));
-		printf("%lu", aft2 % l);
+		printf(", %lu", aft1);
+		printf("%09lu", aft2);
 		aft1 = aft1 + bef1;
 		bef1 = aft1 - bef1;
 		aft2 = aft2 + bef2;
 		bef2 = aft2 - bef2;
+
+		if (aft2 >= 1)
+		{
+			aft2 -= 1;
+			aft1 += 1;
+		}
 	}
 	printf("\n");
 	return (0);
