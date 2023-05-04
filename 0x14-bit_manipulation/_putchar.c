a266adbd54737d7f96ab8ae7b9eb89a6ae387c37
IanKiguru <iankiguru72@gmail.com>
2023-05-04 04:36:24 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+*_putchar - prints char the console
+*@c : the char being printed to the console
+*Return: the printed char
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+
+	return (0);
+}
